51fff72c13a391c82db482e6b7ccdc102907cea5
『Root』 <Root@xn--36jc.st>
2022-04-25 14:52:09 -0400

    updated LightGunnerClass to use new PostInit()

--- Cards/LightGunner/LightGunnerClass.cs
+++ Cards/LightGunner/LightGunnerClass.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnboundLib.Cards;
+using UnboundLib.GameModes;
 
 namespace Ported_FFC.Cards.LightGunner
 {
@@ -19,9 +20,20 @@ namespace Ported_FFC.Cards.LightGunner
             while (classCard == null) yield return null;
             CustomCard.BuildCard<FastMags>((card) => ClassesRegistry.Regester(card, CardType.Card, classCard));
             CustomCard.BuildCard<BattleExperience>((card) => ClassesRegistry.Regester(card, CardType.Card, classCard));
-            CustomCard.BuildCard<Lmg>((card) => ClassesRegistry.Regester(card, CardType.SubClass, classCard));
-            CustomCard.BuildCard<Dmr>((card) => ClassesRegistry.Regester(card, CardType.SubClass, classCard));
-            CustomCard.BuildCard<AssaultRifle>((card) => ClassesRegistry.Regester(card, CardType.SubClass, classCard));
+            CustomCard.BuildCard<Lmg>((card) => ClassesRegistry.Regester(card, CardType.Card, classCard));
+            CustomCard.BuildCard<Dmr>((card) => ClassesRegistry.Regester(card, CardType.Card, classCard));
+            CustomCard.BuildCard<AssaultRifle>((card) => ClassesRegistry.Regester(card, CardType.Card, classCard));
+        }
+
+        public override IEnumerator PostInit()
+        {
+            CardInfo lmg = ModdingUtils.Utils.Cards.instance.GetCardWithName("LMG");
+            CardInfo dmr = ModdingUtils.Utils.Cards.instance.GetCardWithName("DMR");
+            CardInfo assaultRifle = ModdingUtils.Utils.Cards.instance.GetCardWithName("Assault Rifle");
+            ClassesRegistry.Get(lmg).Blacklist(dmr).Blacklist(assaultRifle);
+            ClassesRegistry.Get(dmr).Blacklist(lmg).Blacklist(assaultRifle);
+            ClassesRegistry.Get(assaultRifle).Blacklist(dmr).Blacklist(lmg);
+            yield break;
         }
     }
 }
